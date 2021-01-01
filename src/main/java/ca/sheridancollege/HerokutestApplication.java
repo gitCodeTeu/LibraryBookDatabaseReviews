a268436a20cfb98f77fe01e78bcf002e30991b31
gitCodeTeu <marjorie.teu@gmail.com>
2020-12-31 21:58:44 -0500

    Add files via upload

---
+++ src/main/java/ca/sheridancollege/HerokutestApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokutestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokutestApplication.class, args);
+	}
+
+}
